fbc3821d8923e3e99487a56c9eb8944c871fa4eb
Milroy Fernando <mfernand@lk.ibm.com>
2017-03-08 10:28:56 -0600

    cc

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var twilio = require("twilio");
+var service = cfenv.getAppEnv().getService("twilio");
+var twilio_auth_token = service.credentials.authToken;
+var doctor = require("../lib/doctor.js");
+
+app.post("/sms", twilio.webhook(twilio_auth_token), function (req, res) {
+
+  console.log("sms.."+req.body.MessageSid);
+  var twiml = new twilio.TwimlResponse();
+
+  console.log(req.body.MessageSid + " QUESTION: " + req.body.Body);
+  doctor.ask(req.body.Body, function (answer) {
+    console.log(req.body.MessageSid + " ANSWER: " + answer);
+
+    twiml.message(answer);
+    res.send(twiml);
+  });
+
+});
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
