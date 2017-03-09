a9edf656c73ea7b6598602aa4e186eb44694270b
Milroy Fernando <mfernand@lk.ibm.com>
2017-03-09 04:31:15 -0600

    cc

--- app.js
+++ app.js
@@ -25,11 +25,80 @@ var twilio = require("twilio");
 var service = cfenv.getAppEnv().getService("twilio");
 var twilio_auth_token = service.credentials.authToken;
 //var doctor = require("../lib/doctor.js");
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+var contexts = [];
 
-app.post("/sms", twilio.webhook(twilio_auth_token), function (req, res) {
+var conversation = new ConversationV1({
+		username:'581d14e8-198b-4762-ba83-06fc2f3cfeb8',
+		password:'MJsgvgGvgNro',
+		version_date:'2016-10-21'
+	});
 
-  console.log("sms.."+req.body.MessageSid);
-  var twiml = new twilio.TwimlResponse();
+	
+app.get("/sms", twilio.webhook(twilio_auth_token), function (req, res) {
+  	var message = req.query.Body;
+	var number = req.query.From;
+	var twilionumber = req.query.To;
+	
+	var context = null;
+	var index = 0;
+	var contextIndex = 0;
+	contexts.forEach(function(value){
+		console.log(value.from);
+		if (value.from === number){
+			context = value.context;
+			contextIndex = index;
+		}
+		index = index + 1;
+	});
+	
+	console.log('Received message from '+ number + 'saying\''+ message +'\'');
+	console.log(JSON.stringify(context));
+	
+		conversation.message({
+		input:{text:message},
+		workspace_id:'0c351ca9-1092-4078-823f-69726c911589',
+		context:context
+	},function(err,response){
+		if(err){
+			console.error(err);
+		} else {
+			console.log(response.output.text[0]);
+			if(context===null){
+				contexts.push({'from':number,'context':response.context});
+			} else {
+				contexts[contextIndex].context=response.context;
+			}
+			
+			var intent = response.intents[0].intent;
+			console.log(intent);
+			if(intent==="done"){
+				contexts.splice(contextIndex,1);
+			}
+			
+			/*var client = require('twilio')(
+			'ACcb05d8968a6f4d6a3bfd56811d696c19',
+			'6af780368fe871e2dab4955571a5256b'
+			);
+			
+			client.messages.create({
+				from:twilionumber,
+				to:number,
+				body:response.output.text[0]
+			},function(err,message){
+				if(err){
+				console.error(err.message);
+			}
+
+			});*/
+			console.log("sms.."+req.body.MessageSid);
+  			var twiml = new twilio.TwimlResponse();
+  			twiml.message(response.output.text[0]);
+    		res.send(twiml);
+		}
+	});
+	res.send('');
+
 
   console.log(req.body.MessageSid + " QUESTION: " + req.body.Body);
   /*doctor.ask(req.body.Body, function (answer) {
